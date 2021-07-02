e1622991b240987762dcd050687938b480b45851
Killerlord1988 <killerlord1988@gmail.com>
2021-07-02 19:43:39 +0400

    add promotions

--- build/js/script.js
+++ build/js/script.js
@@ -1,2 +1,26 @@
 'use strict';
 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".trainers__slider-control--next",
+    prevEl: ".trainers__slider-control--prev",
+  },
+
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      slidesPerGroup: 2,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      slidesPerGroup: 4,
+    },
+  }
+});
