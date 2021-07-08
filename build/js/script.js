2aca022c1c6d2ed1381aa608de8015da252d4bfa
Killerlord1988 <killerlord1988@gmail.com>
2021-07-08 19:00:20 +0400

    add mobile timetable

--- build/js/script.js
+++ build/js/script.js
@@ -82,38 +82,17 @@ table.addEventListener('click', handleTableClick);
 //timetable mobile
 
 
-<<<<<<< HEAD
-var days = document.querySelectorAll('.timetable__table-day')
-var colomns = document.querySelectorAll('.timetable__table-day-classes-list')
-var overley = document.querySelector('.timetable__table-time-overley')
-console.log(days);
-
-var handleTableClick2 = (evt) => {
-  var target = evt.target
-  var parent = target.parentNode
-
-  function addActiveClasses(el, cls) {
-    el.classList.add(cls)
-  }
-
-
-  if (target.hasAttribute('data-day')) {
-    days.forEach( el => {
-      addActiveClasses(el, activeDayListClassName)
-    })
-    colomns.forEach( el => {
-      addActiveClasses(el, activeClassesListClassName)
-    })
-    addActiveClasses(overley, activeOverlayClassName)
-    console.log('sdd');
-=======
 const activeOverlayClassName = 'timetable__table-time-overley--active';
 const activeClassesListClassName = 'timetable__table-day-classes-list--active';
 const activeDayListClassName = 'timetable__table-day--active-list';
+const activeArrowClassName = 'timetable__table-day-arrow--active';
 
-const days = document.querySelectorAll('.timetable__table-day')
-const colomns = document.querySelectorAll('.timetable__table-day-classes-list')
-const overley = document.querySelector('.timetable__table-time-overley')
+
+const days = document.querySelectorAll('.timetable__table-day');
+const colomns = document.querySelectorAll('.timetable__table-day-classes-list');
+const overley = document.querySelector('.timetable__table-time-overley');
+const containerDay = document.querySelector('.timetable__table-day-classes');
+console.log(containerDay);
 
 function addActiveClass(el, cls) {
   el.classList.add(cls)
@@ -125,56 +104,69 @@ function removeActiveClass(el, cls) {
 
 let isSelectShown = false
 
-const handleTableClick2 = (evt) => {
-  if (isSelectShown) return
-
-  const target = evt.target
-
-  currentActive = target
-
-  if (target.hasAttribute('data-day')) {
-    days.forEach(el => {
-      addActiveClass(el, activeDayListClassName)
-    })
-
-    colomns.forEach(el => {
-      addActiveClass(el, activeClassesListClassName)
-    })
-
-    addActiveClass(overley, activeOverlayClassName)
-
-    isSelectShown = true
->>>>>>> 8b8562efcd097c0fc3d6a33e6b857876dea27207
-  }
-}
-
-table.addEventListener('click', handleTableClick2);
-
-<<<<<<< HEAD
-=======
 overley.addEventListener('click', ev => {
   removeActiveClass(overley, activeOverlayClassName)
+  removeActiveClass(containerDay, activeArrowClassName)
+
 
   Array.from(days).forEach(el => {
     removeActiveClass(el, activeDayListClassName)
+    removeActiveClass(containerDay, activeArrowClassName)
   })
 
-  Array.from(colomns).filter(c => c !== currentActive.parentNode).forEach( el => {
+  Array.from(colomns).filter(c => c !== currentActive.parentNode).forEach(el => {
     removeActiveClass(el, activeClassesListClassName)
+    removeActiveClass(containerDay, activeArrowClassName)
   })
+
+  isSelectShown = !isSelectShown
 })
 
 days.forEach(day => {
   day.addEventListener('click', evt => {
-    removeActiveClass(overley, activeOverlayClassName)
+    if (!isSelectShown) {
+      const target = evt.target
 
-    Array.from(days).forEach(el => {
-      removeActiveClass(el, activeDayListClassName)
-    })
+      currentActive = target
 
-    Array.from(colomns).filter(c => c !== evt.target.parentNode).forEach( el => {
-      removeActiveClass(el, activeClassesListClassName)
-    })
+      addActiveClass(containerDay, activeArrowClassName)
+
+
+      if (target.hasAttribute('data-day')) {
+        days.forEach(el => {
+          addActiveClass(el, activeDayListClassName)
+        })
+
+        colomns.forEach(el => {
+          addActiveClass(el, activeClassesListClassName)
+        })
+
+        addActiveClass(overley, activeOverlayClassName)
+
+        isSelectShown = true
+      }
+
+    } else {
+
+      removeActiveClass(overley, activeOverlayClassName)
+      removeActiveClass(containerDay, activeArrowClassName)
+
+
+      Array.from(days).forEach(el => {
+        removeActiveClass(el, activeDayListClassName)
+        removeActiveClass(containerDay, activeArrowClassName)
+
+      })
+
+      Array.from(colomns).filter(c => c !== evt.target.parentNode).forEach(el => {
+        removeActiveClass(el, activeClassesListClassName)
+        removeActiveClass(containerDay, activeArrowClassName)
+
+      })
+
+      isSelectShown = false
+    }
+
+
   })
 })
->>>>>>> 8b8562efcd097c0fc3d6a33e6b857876dea27207
